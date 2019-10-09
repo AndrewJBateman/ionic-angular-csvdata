ee95c92734626b5ee29f8dc74ffb747d92031f44
Andrew Bateman <gomezbateman@yahoo.com>
2019-10-09 14:45:34 +0200

    intial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {PapaParseModule} from 'ngx-papaparse';
+import { File } from '@ionic-native/file/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+	declarations: [AppComponent],
+	entryComponents: [],
+	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PapaParseModule, HttpClientModule],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+		File,
+		SocialSharing
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { Papa } from 'ngx-papaparse';
+import { File } from '@ionic-native/file/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
+@Component({
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+	csvData: any[] = [];
+	headerRow: any[] = [];
+
+	constructor(
+		private http: HttpClient,
+		private papa: Papa,
+		private plt: Platform,
+		private file: File,
+		private socialSharing: SocialSharing
+	) {
+		this.loadCSV();
+	}
+
+	private loadCSV() {
+		this.http.get('./assets/test.csv', {
+			responseType: 'text'
+		}).subscribe(
+			data => this.extractData(data),
+			err => console.log('error: ', err)
+		);
+	}
+
+	extractData(res: any) {
+		const csvData = res || '';
+
+		this.papa.parse(csvData, {
+			complete: parsedData => {
+				this.headerRow = parsedData.data.splice(0, 1)[0];
+				this.csvData = parsedData.data;
+				console.log('my array: ', this.csvData);
+			}
+		});
+	}
+
+	trackByFn(index: any, item: any) {
+		return index;
+	}
+
+	exportCSV() {
+		const csv = this.papa.unparse({
+			fields: this.headerRow,
+			data: this.csvData
+		});
+		console.log('csv: ', csv);
+
+		if (this.plt.is('cordova')) {
+			this.file.writeFile(this.file.dataDirectory, 'data.csv', csv, { replace: true }).then(res => {
+				this.socialSharing.share(null, null, res.nativeURL, null);
+				// .then(e => {
+				// 	// success
+				// }).catch(e => {
+				// 	console.log('share failed: ', e);
+				// });
+			});
+		} else {
+			// Dummy implementation for Desktop download purpose
+			const blob = new Blob([csv]);
+			const a = window.document.createElement('a');
+			a.href = window.URL.createObjectURL(blob);
+			a.download = 'newdata.csv';
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+		}
+	}
+
+}
