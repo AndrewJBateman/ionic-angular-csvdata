c414ad02052a4c6e7f8b265870af8941b61d50b0
Andrew Bateman <gomezbateman@yahoo.com>
2023-01-06 10:34:51 +0100

    chore: dependencies updated, update ng code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ const routes: Routes = [
 	{ path: "", redirectTo: "home", pathMatch: "full" },
 	{
 		path: "home",
-		loadChildren: () =>
-			import("./home/home.module").then((m) => m.HomePageModule),
+		loadChildren: () => import("./home/home.module"),
 	},
 ];
 
